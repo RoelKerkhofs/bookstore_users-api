785ed377d57fc8c79241cc70efd15ff26eaac1e4
roelkerkhofs <r_kerkhofs@hotmail.com>
2020-02-18 21:13:46 +0100

    Handle json payload

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,18 +1,36 @@
 package users
 
 import (
-  "github.com/gin-gonic/gin"
-  "net/http"
+	"bookstore/bookstore_users-api/domain/users"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-  c.String(http.StatusNotImplemented, "Create User Route")
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		//TODO handle error
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		//TODO handle json error
+		return
+	}
+
+	c.String(http.StatusNotImplemented, string(bytes))
 }
 
+/*
 func GetUser(c *gin.Context) {
   c.String(http.StatusNotImplemented, "Get User Route")
 }
+*/
 
 func SearchUser(c *gin.Context) {
-  c.String(http.StatusNotImplemented, "Search User Route")
+	c.String(http.StatusNotImplemented, "Search User Route")
 }
