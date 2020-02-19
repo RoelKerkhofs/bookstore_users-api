4b267735a7fbe1cd593c6ede79b4bb01976103c5
roelkerkhofs <r_kerkhofs@hotmail.com>
2020-02-19 21:04:48 +0100

    Added rudimentary persistence

---
+++ domain/users/user_dao.go
@@ -0,0 +1,37 @@
+package users
+
+import (
+	"bookstore/bookstore_users-api/utils/errors"
+	"fmt"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get(userId int64) *errors.RestErr {
+	result := usersDB[userId]
+	if result == nil {
+		return errors.NewBadRequestError(fmt.Sprintf("user id %d not found", userId))
+	}
+
+	user.ID = result.ID
+	user.FirstName = result.FirstName
+	user.LastName = result.LastName
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+
+	return nil
+}
+
+func (user *User) Save() *errors.RestErr {
+	current := usersDB[user.ID]
+	if current != nil {
+		if current.Email == user.Email {
+			return errors.NewBadRequestError(fmt.Sprintf("Email address %s is already registered", user.Email))
+		}
+		return errors.NewBadRequestError(fmt.Sprintf("User id %d already exists", user.ID))
+	}
+	usersDB[user.ID] = user
+	return nil
+}
