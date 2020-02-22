a9f28189ce41c64c58c8e6343f370662e3c9c24b
roelkerkhofs <r_kerkhofs@hotmail.com>
2020-02-22 12:54:03 +0100

    Write to database

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -5,10 +5,16 @@ import (
 	"bookstore/bookstore_users-api/utils/date_utils"
 	"bookstore/bookstore_users-api/utils/errors"
 	"fmt"
+	"strings"
 )
 
 var (
 	usersDB = make(map[int64]*User)
+)
+
+const (
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email_address, date_created) VALUES (?, ?, ?, ?);"
+	indexUniqueEmail = "email_address_UNIQUE"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -32,16 +38,27 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.ID]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("Email address %s is already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("User id %d already exists", user.ID))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
 
-	usersDB[user.ID] = user
+	fmt.Println("payload", user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to save user: %s", err.Error()))
+	}
+	user.ID = userId
 	return nil
 }
--- utils/date_utils/date_utils.go
+++ utils/date_utils/date_utils.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	apiDatelayout string = "2019-01-02T15:04:05Z"
+	apiDatelayout string = "2006-01-02T15:04:05Z"
 )
 
 func GetNow() time.Time {
