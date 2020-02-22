46de60f5c5894947c1f11c1dd75bccbb9da7b608
roelkerkhofs <r_kerkhofs@hotmail.com>
2020-02-22 10:36:15 +0100

    Get rid of dummy credentials

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -5,18 +5,29 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"os"
+)
+
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host     = "mysql_users_host"
+	mysql_users_schema   = "mysql_users_schema"
 )
 
 var (
 	Client *sql.DB
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host     = os.Getenv(mysql_users_host)
+	schema   = os.Getenv(mysql_users_schema)
 )
 
 func init() {
+
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root",
-		"password",
-		"127.0.0.1",
-		"users_db",
+		username, password, host, schema,
 	)
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
